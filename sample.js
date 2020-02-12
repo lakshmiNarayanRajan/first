ee88ce870f7daad2f9218e25af85ebc31feb985f
administrator <you@example.com>
2020-02-12 09:20:43 +0530

    incomplete

--- sample.js
+++ sample.js
@@ -1,1 +1,13 @@
-#lakshmi_rajan
+
+var person1= [ {Name:"John", age:15, class:"fifth",
+    display :function() { log(this.Name + "this.class" + "age");},{Name:"Sai", age:12, class:"sixth",
+    display :function() { log(this.Name + this.class + this.age);},{Name:"Anju", age:14, class:"ninth",
+    display :function() { log(this.Name + this.class + this.age);},{Name:"Sam", age:11, class:"fifth",
+    display :function() { log(this.Name + this.class + this.age);},{Name:"Vaidu", age:15, class:"tenth",
+    display :function() { log(this.Name + this.class + this.age);},{Name:"Febin", age:8, class:"third",
+    display :function() { log(this.Name + this.class + this.age);}];
+
+for(i=0; i<5; i++)
+{
+ a [i]= person1[i];
+}
